571d14e912b53473908a3a6cdb8d4c7f126e1d1e
sachin.dwivedi <sachindwivedi1991@outlook.com>
2025-05-10 01:31:07 +0530

    Added *ngFOr with Array and Array of objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sample Angular 17 App';
-  workingHours : number = 9;
-  IsActive :  boolean = true;
-  salary : number = 3000.59;
-  imgUrl:string = "assets/logo.png";
-  name : string = "";
-  btnClick() : void{
+  workingHours: number = 9;
+  IsActive: boolean = true;
+  salary: number = 3000.59;
+  imgUrl: string = "assets/logo.png";
+  name: string = "";
+  btnClick(): void {
     this.salary = 10000.40;
   }
+
+  course = ["ASP.NET", "C#.NET", "SQL Server", 1];
+
+  subject = [
+    {
+      subcode: 101,
+      name: "Asp.Net Core"
+    },
+    {
+      subcode: 102,
+      name: "Microservice"
+    },
+    {
+      subcode: 103,
+      name: "C#"
+    },
+    {
+      subcode: 104,
+      name: "Entity Framework"
+    },
+    {
+      subcode: 105,
+      name: "Angular 17"
+    },
+  ]
 }
